1bcbafd25652858de0dfaca85e8cde593a307892
Fedor Shaporov <shaporovfed@gmail.com>
2025-12-23 22:37:35 +0300

    сделал покрытие теста

--- src/main.py
+++ src/main.py
@@ -10,13 +10,11 @@ class Category:
     category_count = 0
     product_count = 0
 
-
     def __init__(self, name: str, description: str):
         self.name = name
         self.description = description
         self.products = []
         Category.category_count += 1
-
 
     def add_product(self, product: Product):
         self.products.append(product)
