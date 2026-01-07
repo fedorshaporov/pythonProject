1856abb0681a946841d75c0027c2c7390b15c7da
Fedor Shaporov <shaporovfed@gmail.com>
2026-01-07 11:35:58 +0300

    покрытие 76%

--- tests/test_main.py
+++ tests/test_main.py
@@ -53,3 +53,23 @@ def test_product_addition_type_error():
     grass = LawnGrass("Газонная трава", "Описание", 1500, 30, "Россия", 14, "зеленый")
     with pytest.raises(TypeError):
         smartphone + grass  # Ожидаем ошибку при сложении разных типов
+
+def test_product_setter_price_negative():
+    product = Product("Товар", "Описание товара", 100.0, 10)
+    product.price = -50  # Установим отрицательную цену
+    assert product.price == 100.0  # Убедитесь, что цена не изменилась
+
+def test_product_setter_price_zero():
+    product = Product("Товар", "Описание товара", 100.0, 10)
+    product.price = 0  # Установим нулевую цену
+    assert product.price == 100.0  # Убедитесь, что цена не изменилась
+
+def test_product_addition_within_same_class():
+    smartphone1 = Smartphone("Смартфон 1", "Описание 1", 20000, 5, 5, "Model X1", 128, "синий")
+    smartphone2 = Smartphone("Смартфон 2", "Описание 2", 25000, 2, 8, "Model X2", 256, "черный")
+    assert smartphone1 + smartphone2 == (smartphone1.price * smartphone1.quantity) + (smartphone2.price * smartphone2.quantity)
+
+def test_lawn_grass_attributes():
+    grass = LawnGrass("Груша газонная", "Газонная трава", 1500, 30, "Россия", 14, "зеленый")
+    assert grass.country == "Россия"
+    assert grass.germination_period == 14
